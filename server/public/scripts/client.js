ac2b766784de1e4c11a896f95d1f1b88bfa0ee67
Matt Nelson <mattnelson6992@gmail.com>
2022-04-28 10:55:21 -0500

    operator buttons functional

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,17 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('JQ');
+    $('#inputOperators').on('click', '.operatorButtons', defineOperator);
+}
+
+let toCalculate = {
+    numerator: "",
+    operator: "",
+    denominator: ""
+}
+
+function defineOperator() {
+    console.log('in defineOperator');
+    toCalculate.operator = $(this).data('op');
+    console.log(toCalculate);
 }
