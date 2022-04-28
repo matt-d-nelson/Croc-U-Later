6608746a85e123537e0880089c575c5801332622
Matt Nelson <mattnelson6992@gmail.com>
2022-04-28 11:03:46 -0500

    clearInputs and calculateOperation buttons recieving input

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,6 +3,8 @@ $(document).ready(onReady);
 function onReady() {
     console.log('JQ');
     $('#inputOperators').on('click', '.operatorButtons', defineOperator);
+    $('#clearButton').on('click', clearInputs);
+    $('#calculateButton').on('click', calculateOperation);
 }
 
 let toCalculate = {
@@ -14,5 +16,15 @@ let toCalculate = {
 function defineOperator() {
     console.log('in defineOperator');
     toCalculate.operator = $(this).data('op');
-    console.log(toCalculate);
+}
+
+function clearInputs() {
+    console.log('in clearInputs');
+    $('#numeratorIn').val('');
+    $('#denominatorIn').val('');
+    toCalculate.operator = "";
+}
+
+function calculateOperation() {
+    console.log('in calculateOperation');
 }
