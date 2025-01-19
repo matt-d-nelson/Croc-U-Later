9561f2e7592e95139864d91c7c7793a3a788a9f6
Matt Nelson <matt.dav.nelson@gmail.com>
2025-01-19 13:29:56 -0600

    fix port for heroku

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const calcRouter = require("./modules/calculator/calculator");
 
 //Constants
 const app = express();
-const port = 5001;
+const port = process.env.PORT || 5001;
 
 //app configuration
 app.listen(port, () => {
